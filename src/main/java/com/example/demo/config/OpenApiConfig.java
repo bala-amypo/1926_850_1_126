702423b59a4f60190731cabd066ff811fbcc2ad4
bala-amypo <bala@amypo.in>
2025-12-21 17:05:29 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,32 +1,7 @@
 package com.example.demo.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class OpenApiConfig {
-    
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-            .info(new Info()
-                .title("Customer Loyalty Tier Upgrader API")
-                .version("1.0")
-                .description("API for managing customer loyalty tiers and upgrades"))
-            .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
-            .components(new io.swagger.v3.oas.models.Components()
-                .addSecuritySchemes("Bearer Authentication", 
-                    new SecurityScheme()
-                        .type(SecurityScheme.Type.HTTP)
-                        .scheme("bearer")
-                        .bearerFormat("JWT")));
-    }
-}
-
+package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
@@ -42,7 +17,7 @@ public class OpenApiConfig {
         return new OpenAPI()
                 // You need to change the port as per your server
                 .servers(List.of(
-                        new Server().url("https://9241.pro604cr.amypo.ai/")
+                        new Server().url("https://9244.pro604cr.amypo.ai/")
                 ));
         }
 }
